7395a8eccf3cef791da341dce05065f8a11581df
geolffrey <geolffrey@gmail.com>
2020-02-08 00:01:43 -0500

    Change SVG icons

--- packages/connector-node-v1/src/icons.js
+++ packages/connector-node-v1/src/icons.js
@@ -1,39 +1,64 @@
-import icons from './icons-svg';
+'use strict';
 
-const dirIcon = icons.folder;
-const soundFileIcon = icons.volumeUp;
-const pictureFileIcon = icons.image;
-const videoFileIcon = icons.ondemandVideo;
-const archiveFileIcon = icons.archive;
-const booksFileIcon = icons.book;
-const unknownFileIcon = icons.insertDriveFile;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getIcon = getIcon;
 
-const defaultFillColor = '#424242';
-const soundFilesExtensions = ['aac', 'aiff', 'flac', 'm4a', 'ogg', 'mp3', 'wav', 'wma'];
-const pictureFilesExtensions = ['gif', 'png', 'jpg', 'jpeg', 'bmp', 'svg'];
-const videoFilesExtensions = ['avi', 'flv', 'wmv', 'mov', 'mp4'];
-const archiveFilesExtensions = ['tar', 'zip', 'gz', 'bz2', 'rar'];
-const booksFilesExtensions = ['pdf', 'epub', 'fb2'];
+var _iconsSvg = require('./icons-svg');
+
+var _iconsSvg2 = _interopRequireDefault(_iconsSvg);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var dirIcon = _iconsSvg2.default.folder;
+var soundFileIcon = _iconsSvg2.default.volumeUp;
+var pictureFileIcon = _iconsSvg2.default.image;
+var videoFileIcon = _iconsSvg2.default.ondemandVideo;
+var archiveFileIcon = _iconsSvg2.default.archive;
+var booksFileIcon = _iconsSvg2.default.book;
+var wordFileIcon = _iconsSvg2.default.word;
+var excelFileIcon = _iconsSvg2.default.excel;
+var powerpointFileIcon = _iconsSvg2.default.powerpoint;
+var unknownFileIcon = _iconsSvg2.default.insertDriveFile;
+
+var defaultFillColor = '#424242';
+var soundFilesExtensions = ['aac', 'aiff', 'flac', 'm4a', 'ogg', 'mp3', 'wav', 'wma'];
+var pictureFilesExtensions = ['gif', 'png', 'jpg', 'jpeg', 'bmp', 'svg','tif'];
+var videoFilesExtensions = ['avi', 'flv', 'wmv', 'mov', 'mp4'];
+var archiveFilesExtensions = ['tar', 'zip', 'gz', 'bz2', 'rar'];
+var booksFilesExtensions = ['pdf', 'epub', 'fb2'];
+var wordFilesExtensions = ['doc', 'docx'];
+var excelFilesExtensions = ['xls', 'xlsx','xlsm'];
+var powerpointFilesExtensions = ['ppt', 'pptx','pptm'];
 
 function matchFileExtensions(filename, extensions) {
-  const extensionsRegExp = `(${extensions.join('|')})`;
-  return extensions.some((o) => new RegExp(`^.*\.${extensionsRegExp}$`).test(filename.toLowerCase()));
+  var extensionsRegExp = '(' + extensions.join('|') + ')';
+  return extensions.some(function (o) {
+    return new RegExp('^.*.' + extensionsRegExp + '$').test(filename.toLowerCase());
+  });
 }
 
-export function getIcon(resource) {
+function getIcon(resource) {
   if (resource.type === 'dir') {
     return { svg: dirIcon, fill: defaultFillColor };
   } else if (matchFileExtensions(resource.name, soundFilesExtensions)) {
-    return { svg: soundFileIcon, fill: `#e53935` };
+    return { svg: soundFileIcon, fill: '#e53935' };
   } else if (matchFileExtensions(resource.name, pictureFilesExtensions)) {
-    return { svg: pictureFileIcon, fill: `#e53935` };
+    return { svg: pictureFileIcon, fill: '#e53935' };
   } else if (matchFileExtensions(resource.name, videoFilesExtensions)) {
-    return { svg: videoFileIcon, fill: `#e53935` };
+    return { svg: videoFileIcon, fill: '#e53935' };
   } else if (matchFileExtensions(resource.name, archiveFilesExtensions)) {
-    return { svg: archiveFileIcon, fill: `#616161` };
+    return { svg: archiveFileIcon, fill: '#616161' };
   } else if (matchFileExtensions(resource.name, booksFilesExtensions)) {
-    return { svg: booksFileIcon, fill: `#e53935` };
+    return { svg: booksFileIcon, fill: '#e53935' };
+  } else if (matchFileExtensions(resource.name, wordFilesExtensions)) {
+    return { svg: wordFileIcon, fill: '#e53935' }; 	
+  } else if (matchFileExtensions(resource.name, excelFilesExtensions)) {
+    return { svg: excelFileIcon, fill: '#e53935' }; 		
+  } else if (matchFileExtensions(resource.name, powerpointFilesExtensions)) {
+    return { svg: powerpointFileIcon, fill: '#e53935' }; 			
   } else {
-    return { svg: unknownFileIcon, fill: `#616161` };
+    return { svg: unknownFileIcon, fill: '#616161' };
   }
 }
